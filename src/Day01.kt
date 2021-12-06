e23f2a87b314644a125def1755eb1df7a9bad6ea
Yuku on yuku10 <yukuku@gmail.com>
2021-12-06 11:46:50 +0800

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var res = 0
+        for (i in 1 until input.size) {
+            if (input[i] > input[i-1]) {
+                res++
+            }
+        }
+        return res
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var res = 0
+        for (i in 3 until input.size) {
+            if (input[i]+input[i-1]+input[i-2] > input[i-1]+input[i-2]+input[i-3]) {
+                res++
+            }
+        }
+        return res
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readIntInput("Day01_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readIntInput("Day01")
     println(part1(input))
     println(part2(input))
 }
